3d53bc99a3c600133ca306245b7d1795c493aabd
nofurtherinformation <dhalpern@uchicago.edu>
2024-07-26 16:46:18 -0500

    devops

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
   define: {
-    'procces.env': {
-      'VITE_DATA_ENDOINT': ''
-    }
-  }
-})
+    "procces.env": {
+      VITE_DATA_ENDOINT: "",
+    },
+  },
+  optimizeDeps: {
+    include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
+  },
+});

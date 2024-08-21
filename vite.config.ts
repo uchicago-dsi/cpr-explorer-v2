ba2b99f5f33665158bf2d33544e69ef3bb9df395
nofurtherinformation <dhalpern@uchicago.edu>
2024-08-21 09:43:07 -0500

    meta - remove favicon etc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// @ts-ignore
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +12,14 @@ export default defineConfig({
         plugins: ["@emotion/babel-plugin"],
       },
     }),
+
+    visualizer({
+      template: "treemap", // or sunburst
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: "analyze.html", // will be saved in project's root
+    })
   ],
   define: {
     "procces.env": {
